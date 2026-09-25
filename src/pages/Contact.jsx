3ddd69eb2fa30fef660669a1a85dc835e1c383ef
AsyRio <theriohotasy@gmail.com>
2026-09-26 03:35:12 +0700

    mamat gunshop reborn

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ function Contact() {
     <div className="page">
       <h1 className="display">Come by the shop.</h1>
       <p className="lede">
-        123 Range Road. Open Tuesday to Saturday, 10 to 6. Call (555) 010-0100.
+        Burjo ST ST S nya Senapan T nya Tembak.
       </p>
     </div>
   )
